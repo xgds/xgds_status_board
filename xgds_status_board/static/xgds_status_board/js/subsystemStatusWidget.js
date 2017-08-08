204172cecc30f813b6533298adbbabbd3aa2acf5
Tamar Cohen <tamar.cohen@nasa.gov>
2017-08-08 10:57:52 -0700

    tuning status board display

--- xgds_status_board/static/xgds_status_board/js/subsystemStatusWidget.js
+++ xgds_status_board/static/xgds_status_board/js/subsystemStatusWidget.js
@@ -1,10 +1,38 @@
 var xgds_status_board = xgds_status_board || {};
 
+Handlebars.registerHelper('getDisplayValue', function(sub) {
+	var result = '';
+	if (!('name' in sub)){
+		return '';
+	}
+	if (sub['name'].includes('LoadAverage')) {
+		var oneMin = sub['oneMin'];
+		if (oneMin === undefined){
+			oneMin = '';
+		}
+		var fiveMin = sub['fiveMin']
+		if (fiveMin === undefined){
+			fiveMin = '';
+		}
+		result='<td class="status elapsedTime"><table class="loadAverage"><thead><th>1min</th><th>5min</th><th></th></thead>';
+		result += '<tbody><tr id="' + sub["name"] +'_loadAverage"';
+		result += '<td class="oneMin status">' + oneMin + '</td>';
+		result += '<td class="fiveMin status">' + fiveMin + '</td></tr></tbody></table></td>';
+	} else {
+		result = '<td class="status elapsedTime" id="' + sub['name'] +'_status"';
+		if ('statusColor' in sub && (!_.isEmpty(sub['statusColor']))) {
+			result += 'style="background-color:' + sub['statusColor'] + '"';
+		}
+		result += '>' + sub['elapsedTime'] + '</td>';
+	}
+	  return  new Handlebars.SafeString(result);
+});
 
 // builds the UI and polls server for updates for each subsystem group
 $(function() {
 	function Widget(groupName, container, initialData) {
 		this.groupName = groupName;
+		this.container = container;
 		this.initialize(container, initialData)
 		return this;
 	};
@@ -33,7 +61,8 @@ $(function() {
 
 	Widget.prototype.render = function(data) {
 		this.data = this.buildDataObject(data);
-		this.el.html(this.template(this.data));
+		this.el = $(this.template(this.data));
+		this.container.html(this.el);
 	};
 
 	xgds_status_board.SubsystemStatusWidget = Widget;
