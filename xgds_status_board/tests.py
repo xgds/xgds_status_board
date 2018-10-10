564364ae2870c96ea771741fb5d2cb18b26528ef
Tamar Cohen <tamar.cohen@nasa.gov>
2018-10-10 08:17:50 -0700

    replaced TestCase with TransactionTestCase

--- xgds_status_board/tests.py
+++ xgds_status_board/tests.py
@@ -14,10 +14,10 @@
 #specific language governing permissions and limitations under the License.
 # __END_LICENSE__
 
-from django.test import TestCase
+from django.test import TransactionTestCase
 
 
-class xgds_status_boardTest(TestCase):
+class xgds_status_boardTest(TransactionTestCase):
     """
     Tests for xgds_status_board
     """
